fb0365f548dac172823f37a4ebd63eae742f60f0
andresperezceron <aperezlevi@gmail.com>
2018-05-28 00:31:20 +0200

    instalar sqlite3

--- src/index.js
+++ src/index.js
@@ -8,12 +8,14 @@ const server = http.createServer(function (request, response) {
   console.log('me ha entrado una peti por...: ', request.method);//TODO: borrame.
   console.log('desde la url: ', request.url);//TODO: borramoe.
 
-  if ( request.url === "/endpoint") {
-   response.write("smoke en acción");
-  } else {
-    response.write(request.method);
-  }
-  response.end();
+
+    if(request.url === "/endpoint") {
+        response.write("smoke en acción");
+    } else {
+
+        response.write(request.method);
+    }
+    response.end();
 });
 
 server.listen(8888);
