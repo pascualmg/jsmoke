21a9ecf59e397632d953e9473fcb10d09e920077
Pascual <pascual.munoz@pccomponentes.com>
2018-05-28 00:01:42 +0200

    skel para el router.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,13 @@ const http = require('http');
 
 const server = http.createServer(function (request, response) {
   console.log('me ha entrado una peti por...: ', request.method);//TODO: borrame.
-  response.write("hola caracola");
+  console.log('desde la url: ', request.url);//TODO: borramoe.
+
+  if ( request.url === "/endpoint") {
+   response.write("smoke en acción");
+  } else {
+    response.write(request.method);
+  }
   response.end();
 });
 
