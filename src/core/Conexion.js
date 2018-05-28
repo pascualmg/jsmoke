20948c7930b5f31b5f9ba3561dcccd23297d8c0e
andresperezceron <aperezlevi@gmail.com>
2018-05-28 23:51:27 +0200

    configurations

--- src/core/Conexion.js
+++ src/core/Conexion.js
@@ -1,2 +1,17 @@
+var sqlite3 = require('sqlite3').verbose();
 
 
+function Conexion(path) {
+    this.db = this.db || new sqlite3.Database(path);
+    this.exeQuery = function(sql) {
+        return this.db.prepare(sql);
+    };
+
+}
+
+var con = new Conexion('D:/Programacion/Sqlite/zapalevi.db');
+var pro = con.exeQuery("select nombre from proveedor");
+
+/*nombresPro.forEach((item) => {
+   console.log(item.nombre);
+});*/
