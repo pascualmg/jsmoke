a77df819970db39d33d8fba503576e226dc8daaf
andresperezceron <aperezlevi@gmail.com>
2018-05-30 00:28:37 +0200

    investigando y aprendiendo

--- src/core/Conexion.js
+++ src/core/Conexion.js
@@ -2,16 +2,23 @@ var sqlite3 = require('sqlite3').verbose();
 
 
 function Conexion(path) {
-    this.db = this.db || new sqlite3.Database(path);
-    this.exeQuery = function(sql) {
-        return this.db.prepare(sql);
+    this.db = new sqlite3.Database(path);
+    this.executeQuery = function(sql, callback) {
+        this.db.all(sql, (err, row) => {
+           if(row) {
+               console.log(row);
+               callback(row);
+           }
+        });
     };
-
 }
 
-var con = new Conexion('D:/Programacion/Sqlite/zapalevi.db');
-var pro = con.exeQuery("select nombre from proveedor");
 
-/*nombresPro.forEach((item) => {
-   console.log(item.nombre);
-});*/
+
+var con = new Conexion("D:/Programacion/Sqlite/zapalevi.db");
+
+var miResult = con.executeQuery("select * from proveedor", function(row) {
+    console.log(row);
+        return row;
+});
+console.log(miResult);
