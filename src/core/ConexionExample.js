7cc5d56d24d853adcdf63a60b56700016193baff
Pascual <pascual.munoz@pccomponentes.com>
2018-05-29 18:09:12 +0200

    refactor del ConexionExample para que use el module pattern.

--- src/core/ConexionExample.js
+++ src/core/ConexionExample.js
@@ -3,19 +3,47 @@
 import sqlite from 'sqlite3';
 sqlite.verbose();
 
-const db = new sqlite.Database(':memory:');
+function generate() {
+    const db = new sqlite.Database(':memory:');
 
-db.serialize(() => {
-    //Creamos tabla
-    db.run("CREATE TABLE prueba (info TEXT)");
-
-    //llenamos de fixtures.
-    const stmt = db.prepare("INSERT INTO prueba VALUES (?)");
-    for (let i = 0; i < 10; i++) {
-        stmt.run("datosDePrueba" + i);
+    function query(sql){
+        db.serialize(() => {
+            let allResults = [];
+            console.log('procesando sql', sql);//TODO: borrame.
+            db.each(sql,(err, row) => {
+                console.log('procesando...row-->', row );//TODO: borrame.
+                allResults.push(row);
+            });
+            return allResults;
+        });
     }
-    stmt.finalize();
+    async function test() {
+        db.serialize(() => {
+            let result = [];
+            //Creamos tabla
+            db.run("CREATE TABLE prueba (info TEXT)");
 
-    //hacemos una query.
-    db.each("SELECT rowid AS id , info FROM prueba", (err, row) => console.log('result', row.id, " ", row.info));
-});
+            //llenamos de fixtures.
+            const stmt = db.prepare("INSERT INTO prueba VALUES (?)");
+            for (let i = 0; i < 10; i++) {
+                stmt.run("datosDePrueba" + i);
+            }
+            stmt.finalize();
+
+            //hacemos una query.
+            //db.each("SELECT rowid AS id , info FROM prueba", (err, row) => console.log('result', row.id, " ", row.info, result) && result.push(row));
+        })
+    }
+    function close() {
+        console.log('cerrando la conn');//TODO: borrame.
+        console.log('output ', db.close());//TODO: borrame.
+    }
+
+    return {
+        test: test,
+        close: close,
+        query: query,
+    }
+}
+
+export default { generate } ;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,20 @@
  */
 import http from 'http';
 
+import ConexionExample from './core/ConexionExample.js';
+let conn = ConexionExample.generate();
+
+
 const server = http.createServer(function (request, response) {
   console.log('me ha entrado una peti por...: ', request.method);//TODO: borrame.
   console.log('desde la url: ', request.url);//TODO: borramoe.
 
-
     if(request.url === "/endpoint") {
         response.write("smoke en acción");
+        
+        conn.test();
+        console.log( conn.query("SELECT rowid AS id , info FROM prueba") );
+        conn.close();
     } else {
 
         response.write(request.method);
