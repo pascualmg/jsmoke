f9819b456b21fe799b57c8111b722c7148b20b80
andresperezceron <aperezlevi@gmail.com>
2018-05-31 01:02:19 +0200

    rulando ....

--- src/core/Conexion.js
+++ src/core/Conexion.js
@@ -1,24 +1,28 @@
 var sqlite3 = require('sqlite3').verbose();
 
+var db = new sqlite3.Database("D:/Programacion/Sqlite/zapalevi.db");
+var Conexion = {};
 
-function Conexion(path) {
-    this.db = new sqlite3.Database(path);
-    this.executeQuery = function(sql, callback) {
-        this.db.all(sql, (err, row) => {
-           if(row) {
-               console.log(row);
-               callback(row);
-           }
-        });
+
+Conexion.executeQuery = function(sql, callback) {
+    var result = {
+
     };
+    db.each(sql,
+        function item(err, row) {
+            if(row) {
+                result[row.id] = row;
+            }
+        },
+        function complete(){
+            callback(result);
+        });
+};
+
+function putamierda(result) {
+    console.log(JSON.stringify(result));
 }
 
+Conexion.executeQuery("select * from proveedor", putamierda);
 
 
-var con = new Conexion("D:/Programacion/Sqlite/zapalevi.db");
-
-var miResult = con.executeQuery("select * from proveedor", function(row) {
-    console.log(row);
-        return row;
-});
-console.log(miResult);
