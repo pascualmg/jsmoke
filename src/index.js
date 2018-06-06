4e189cd377479cb15560f6f8aa5d8e51a5d9cff5
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2018-06-06 19:32:23 +0200

    feature/express , se refactoriza el index para que que el server sea express , no el http a pelo .

--- src/index.js
+++ src/index.js
@@ -1,33 +1,18 @@
 /**
  * skel para el router , levanta un servidor que
- * escucha peticiones en el puerto 8888.
+ * escucha peticiones en el puerto 8888 con express.
+ *
  */
-import http from 'http';
+import express from 'express';
 
-import ConexionExample from './core/ConexionExample';
-import Conexion from './core/Conexion';
+const app = express();
+let port = process.env.PORT || 8888;
 
-let conn = ConexionExample.generate();
-let con = Conexion.Conexion;
+// import ConexionExample from './core/ConexionExample';
+// import Conexion from './core/Conexion';
 
-
-const server = http.createServer(function (request, response) {
-  console.log('me ha entrado una peti por...: ', request.method);//TODO: borrame.
-  console.log('desde la url: ', request.url);//TODO: borramoe.
-
-    if(request.url === "/endpoint") {
-        response.write("smoke en acción");
-
-        con.executeQuery("select * from tipo", (result) => response.write(result));
-        //conn.test();
-        //console.log("vale" , conn.query("SELECT rowid AS id , info FROM prueba", console.log ));
-        //conn.close();
-    } else {
-
-        response.write(request.method);
-    }
-    response.end();
+app.get('/', (req, res) => {
+    res.json({algo: "datos"});
 });
 
-
-server.listen(8888);
+app.listen(port);
